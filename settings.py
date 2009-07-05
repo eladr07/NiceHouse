fb8a50d8336e16ae045bdb59dd4ecc925c9e7448
Elad Reuveni <eladr07@gmail.com>
2009-07-05 19:26:36 +0000

    added password for mysql
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@78 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ LOGIN_REDIRECT_URL = '/'
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'Management'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_PASSWORD = 'HwMqJY8'         # Not used with sqlite3.
 DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = '3306'             # Set to empty string for default. Not used with sqlite3.
 
