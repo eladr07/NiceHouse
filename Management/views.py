084be57ad1747625ad76e00b189972d0abf3c5ee
Elad Reuveni <eladr07@gmail.com>
2009-08-04 20:02:37 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@302 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -1344,7 +1344,8 @@ def json_buildings(request, project_id):
 
 @login_required
 def json_employees(request, project_id):
-    data = serializers.serialize('json', Project.objects.get(pk= project_id).employees.all())
+    l = [e.employeebase for e in Project.objects.get(pk=project_id).employees.all()]
+    data = serializers.serialize('json', l)
     return HttpResponse(data)
 
 @login_required
