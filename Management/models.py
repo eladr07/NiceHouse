1422f6497923ffa018b4727835c784875f471fd4
Elad Reuveni <eladr07@gmail.com>
2009-08-19 22:08:53 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@444 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1111,7 +1111,7 @@ class ProjectCommission(models.Model):
                                                    object_id=scd_final.id, 
                                                    attribute='value',
                                                    date__lt=finish_date)
-                    if s.contractor_pay.month == 7:
+                    if s.contractor_pay.month == 6:
                         raise AttributeError()
                     if log.count() > 0:
                         paid_final_value = float(log.latest().new_value)
