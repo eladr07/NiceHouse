4cf7ca837e3774ca7b21b094deb35a4310b946c0
Elad Reuveni <eladr07@gmail.com>
2009-08-19 21:36:03 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@435 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1098,6 +1098,7 @@ class ProjectCommission(models.Model):
                                         ).filter(demand__project__id = s.demand.project.id
                                         ).exclude(contractor_pay__gte = date(y, m ,1))
                 for s in subSales:
+                    raise AttributeError()
                     signup = s.house.get_signup()
                     q = Demand.objects.filter(month=signup.date.month,
                                               year = signup.date.year,
