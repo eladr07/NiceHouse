740d64799da39dbf403dfae78047789d892f25c6
Elad Reuveni <eladr07@gmail.com>
2009-08-21 09:58:17 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@538 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1092,6 +1092,9 @@ class ProjectCommission(models.Model):
                                         ).filter(demand__project__id = demand.project.id
                                         ).exclude(contractor_pay__gte = date(demand.month==12 and demand.year+1 or demand.year, 
                                                                              demand.month==12 and 1 or demand.month+1,1))
+                if m = 3 and demand.month==5:
+                    cnt = subSales.count()
+                    raise AttributeError
                 self.calc(subSales, 1)
                 #get sales from last months, affected by this month's calculation,
                 #excluding sales from current demand
