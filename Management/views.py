26e610422d3b7f9ec96ee52706cffed262393d57
Elad Reuveni <eladr07@gmail.com>
2009-08-29 17:12:18 +0000

    typos
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@574 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -411,8 +411,8 @@ def employee_list_pdf(request):
     p.close()
     return response
 
-def nhmonth_sales(request, branch_id, year=datetime.now().year, month=datetime.now().month):
-    nhm = NHMonth.objects.get(branch__id = branch_id, year=year, month=month)
+def nhmonth_sales(request, nhbranch_id, year=datetime.now().year, month=datetime.now().month):
+    nhm = NHMonth.objects.get(nhbranch__id = nhbranch_id, year=year, month=month)
     form = MonthFilterForm(initial={'year':year,'month':month})
     return render_to_response('Management/nhmonth_sales.html', 
                               { 'nhmonth':nhm, 'filterForm':form },
