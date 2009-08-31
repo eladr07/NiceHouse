1015a360b119c8257b113236bd1b956fdd6177a7
Elad Reuveni <eladr07@gmail.com>
2009-08-31 18:15:03 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@614 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1478,7 +1478,6 @@ class Lawyer(Person):
     class Meta:
         db_table='Lawyer'
 
-SaleTypeSell, SaleTypeRent = (1,2)
 class SaleType(models.Model):
     name = models.CharField(max_length=20, unique=True)
     def __unicode__(self):
