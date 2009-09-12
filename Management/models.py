11fc1d427d167fe28dc4a7ec2f1fa0fa6edf95cf
Elad Reuveni <eladr07@gmail.com>
2009-09-12 19:07:10 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@782 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1892,10 +1892,10 @@ class Sale(models.Model):
         return self.restore and finish_date and restore_object(q[0], finish_date).value or q[0].value
     @property
     def c_final(self):
-        if self.restore: raise AttributeError()
         q = self.project_commission_details.filter(commission='final')
         if q.count() == 0: return 0
         finish_date = self.actual_demand.finish_date
+        if not finish_date: raise AttributeError()
         return self.restore and finish_date and restore_object(q[0], finish_date).value or q[0].value
     @property
     def pc_base_worth(self):
