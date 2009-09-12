1a926386f537111fb00471523a51888702b46be4
Elad Reuveni <eladr07@gmail.com>
2009-09-12 21:22:53 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@790 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1130,11 +1130,12 @@ class CZilber(models.Model):
         p = self.projectcommission.project
         d = p.demands.get(year = month.year, month = month.month)
         d.bonus, d.bonus_type, d.var_pay, d.var_pay_type = 0, None, 0, None
-        demand = p.demands.get(year = month.year, month = month.month)
+        demand = d
         sales = []
         while demand != None and demand.zilber_cycle_index() > 0:
             sales.extend(demand.get_sales())
             demand = demand.get_previous_demand()
+        raise AttributeError()
         base = self.base + self.b_sale_rate * (len(sales) - 1)
         if base > self.b_sale_rate_max:
             base = self.b_sale_rate_max
