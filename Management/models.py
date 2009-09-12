6613c61f08c02baebfdbca9568ca4666a9a355c6
Elad Reuveni <eladr07@gmail.com>
2009-09-12 21:33:11 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@795 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1130,7 +1130,8 @@ class CZilber(models.Model):
         p = self.projectcommission.project
         d = p.demands.get(year = month.year, month = month.month)
         d.bonus, d.bonus_type, d.var_pay, d.var_pay_type = 0, None, 0, None
-        demand = p.demands.get(year = month.year, month = month.month)
+        demand = d
+        raise AttributeError()
         sales = []
         i=0
         while demand != None and demand.zilber_cycle_index() > 0:
