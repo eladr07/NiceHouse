52a0a89f3d683bb6ceb875ab4ebd0e51651f1244
Elad Reuveni <eladr07@gmail.com>
2009-09-13 08:44:42 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@828 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/pdf.py
+++ Management/pdf.py
@@ -663,8 +663,8 @@ class NHEmployeeSalariesWriter:
         rows = []
         i = 0
         for es in self.salaries.all():
-            terms = es.employee.employment_terms
-            row = [log2vis(unicode(es.employee)), 
+            terms = es.nhemployee.employment_terms
+            row = [log2vis(unicode(es.nhemployee)), 
                    log2vis(u'%s - %s' % (terms.hire_type.name, terms.salary_net and u'נטו' or u'ברוטו')), 
                    commaise(es.total_amount), commaise(es.check_amount),
                    commaise(es.refund or 0), es.bruto_amount and commaise(es.bruto_amount),
