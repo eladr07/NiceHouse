08f3b14a1b7ba3e01d399ef422a3e882dfd41f77
Elad Reuveni <eladr07@gmail.com>
2009-09-15 21:42:47 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@888 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/forms.py
+++ Management/forms.py
@@ -538,7 +538,7 @@ class DemandReportForm(forms.Form):
     year = forms.ChoiceField(choices=((i,i) for i in range(datetime.now().year - 10, datetime.now().year+10)), 
                              label = ugettext('year'), initial = datetime.now().year)
     month = forms.ChoiceField(choices=((i,i) for i in range(1,13)), label = ugettext('month'),
-                              initial = demand_month().month)
+                              initial = Demand.current_month().month)
 
 class ProjectSeasonForm(forms.Form):
     project = forms.ModelChoiceField(queryset = Project.objects.all(), label=ugettext('project'))
