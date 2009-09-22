eb048a88ed90b18b8ce32666b2327b486de8cbf8
Elad Reuveni <eladr07@gmail.com>
2009-09-22 20:24:35 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1014 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -79,7 +79,6 @@ def limited_create_object(request, permission=None, *args, **kwargs):
     
 @login_required
 def limited_delete_object(request, model, object_id, post_delete_redirect):
-    model = kwargs['model']
     if not permission or request.user.has_perm('Management.delete_' + model.__name__):
         return HttpResponse('Management.delete_' + model.__name__)
         model.delete(object_id)
