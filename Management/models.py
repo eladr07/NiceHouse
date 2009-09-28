c3296b05c7ca8627b61bf148df2ef6eb46a6f0a2
Elad Reuveni <eladr07@gmail.com>
2009-09-28 19:59:50 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1106 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -761,6 +761,9 @@ class EmployeeSalaryBaseStatus(models.Model):
         get_latest_by = 'date'
         
 class EmployeeSalaryBase(models.Model):
+    employee = models.ForeignKey('Employee', verbose_name=ugettext('employee'), related_name='salaries', null=True, blank=True)
+    nhemployee = models.ForeignKey('NHEmployee', verbose_name=ugettext('nhemployee'), related_name='salaries', null=True, blank=True)
+    
     month = models.PositiveSmallIntegerField(ugettext('month'), editable=False, choices=((i,i) for i in range(1,13)))
     year = models.PositiveSmallIntegerField(ugettext('year'), editable=False, choices=((i,i) for i in range(datetime.now().year - 10,
                                                                                              datetime.now().year + 10)))
