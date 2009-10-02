a5e8a3e22b246cac38029f9eb3fbda00250df7a7
Elad Reuveni <eladr07@gmail.com>
2009-10-02 10:16:31 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1134 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -18,7 +18,7 @@ from mail import mail
 
 @login_required
 def index(request):
-    '''
+    
     for p_id in [21]:
         for m in range(1,9):
             q = Demand.objects.filter(project__id = p_id, year = 2009, month=m)
@@ -36,7 +36,7 @@ def index(request):
             d = Demand.objects.get(project__id = p_id, year = 2009, month=m)
             d.finish()
             time.sleep(1)
-    '''
+    
     return render_to_response('Management/index.html',
                               {'locateHouseForm':LocateHouseForm(),
                                'nhbranches':NHBranch.objects.all()},
