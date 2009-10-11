a4a51447b7b579181e423e4ad0d77adbf50bbc08
Elad Reuveni <eladr07@gmail.com>
2009-10-11 12:25:43 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1269 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -1349,7 +1349,7 @@ def abbrevate(s):
 
 @login_required
 def employee_commission_add(request, employee_id, project_id, commission):
-    return getattr(inspect.getmodule(employee_commission_add), 'employee_' + attribute)(request, employee_id, project_id)
+    return getattr(inspect.getmodule(employee_commission_add), 'employee_' + commission)(request, employee_id, project_id)
         
 @permission_required('Management.add_cvarprecentage')
 def project_cvp(request, project_id):
