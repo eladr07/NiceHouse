32f7a4842d6ca4d5eade336be250a8fc12e563b8
Elad Reuveni <eladr07@gmail.com>
2009-11-06 11:23:34 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1635 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -661,7 +661,7 @@ def nh_season_income(request):
     nhbranch = NHBranch.objects.get(pk=nhbranch_id)
     season_income, season_net_income, season_income_notax, season_net_income_notax = 0, 0, 0, 0
     total_avg_signed_commission, total_avg_actual_commission = 0,0
-    q = nhbranch.nhemployees.exclude(work_end__isnull = False, work_end__lt = date(from_year, from_month, 1),
+    q = nhbranch.nhemployees.exclude(work_end__isnull = False, #work_end__lt = date(from_year, from_month, 1),
                                      work_start__gt = date(to_month == 12 and to_year + 1 or to_year,
                                                            to_month == 12 and 1 or to_month + 1, 1))
     employees = list(q)
