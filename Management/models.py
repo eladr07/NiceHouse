520814b136845215bbd1fc414f547ef1e73f9901
Elad Reuveni <eladr07@gmail.com>
2009-11-22 22:38:38 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1760 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -2271,7 +2271,9 @@ class Sale(models.Model):
         return self.include_tax and self.price or self.price * tax
     @property
     def price_taxed_for_perfect_size(self):
-        return float(self.price_taxed) / self.house.perfect_size
+        if not self.house.perfect_size:
+            return 0
+        return self.price_taxed / self.house.perfect_size
     def project_price(self):
         c = self.house.building.project.commissions
         if c.include_lawyer == None:
