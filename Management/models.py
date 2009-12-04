2270416e174550e2ccd15fc20b27c2b47c97641e
Elad Reuveni <eladr07@gmail.com>
2009-12-04 15:30:55 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1884 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1429,13 +1429,12 @@ class ProjectCommission(models.Model):
                                                commission_include=True
                                         ).exclude(contractor_pay__gte = date(demand.month==12 and demand.year+1 or demand.year, 
                                                                              demand.month==12 and 1 or demand.month+1,1))
+                subSales = subSales.order_by('house__signups__date')
                 self.calc(subSales, 1)#send these sales to regular processing
             if demand.bonus_diff: demand.bonus_diff.delete()
             bonus = 0
             for subSales in demand.get_affected_sales().values():
-                sorted_sales = list(subSales)
-                sorted_sales.sort(lambda x,y:x.house.get_signup().date < y.house.get_signup().date) 
-                for s in sorted_sales:
+                for s in subSales:
                     if not s.commission_include: continue
                     signup = s.house.get_signup()
                     if not signup: continue
