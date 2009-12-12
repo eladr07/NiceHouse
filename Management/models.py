36c4a4ea83b86bafc3e7aa14a557868f6d644f67
Elad Reuveni <eladr07@gmail.com>
2009-12-12 15:48:44 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1974 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -572,6 +572,7 @@ class NHCBase(models.Model):
             sales = set(NHSaleSide.objects.filter(nhsale__nhmonth = nhmonth, employee1=self.nhemployee))
             sales.union(NHSaleSide.objects.filter(nhsale__nhmonth = nhmonth, employee2=self.nhemployee))
             sales.union(NHSaleSide.objects.filter(nhsale__nhmonth = nhmonth, employee3=self.nhemployee))
+            raise TypeError
             for nhss in sales:
                 pay = nhss.get_employee_pay(self.nhemployee)
                 all_pay = nhss.all_employee_commission
