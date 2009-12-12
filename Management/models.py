eaaa1dde4a1d17c2af057ac1b9c6706ffcb52402
Elad Reuveni <eladr07@gmail.com>
2009-12-12 15:56:31 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@1977 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -569,10 +569,9 @@ class NHCBase(models.Model):
                                           month = nhmonth.month)
         scds = []
         if self.filter.id == NHSaleFilter.His or self.filter.id == NHSaleFilter.All:
-            sales = set(list(NHSaleSide.objects.filter(nhsale__nhmonth = nhmonth, employee1=self.nhemployee)))
+            sales = set(NHSaleSide.objects.filter(nhsale__nhmonth = nhmonth, employee1=self.nhemployee))
             sales.union(list(NHSaleSide.objects.filter(nhsale__nhmonth = nhmonth, employee2=self.nhemployee)))
             sales.union(list(NHSaleSide.objects.filter(nhsale__nhmonth = nhmonth, employee3=self.nhemployee)))
-            raise TypeError
             for nhss in sales:
                 pay = nhss.get_employee_pay(self.nhemployee)
                 all_pay = nhss.all_employee_commission
