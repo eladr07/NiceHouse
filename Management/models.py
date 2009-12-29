5f0f5d4a0202898d25e33da13b6d542996335673
Elad Reuveni <eladr07@gmail.com>
2009-12-29 19:27:03 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@2080 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -35,6 +35,17 @@ Boolean = (
 RoomsChoices = [(float(i)/2,float(i)/2) for i in range(2, 21)]
 RoomsChoices.insert(0, ('',u'----'))
 
+def nhemployee_sort(nhemployee1, nhemployee2):
+    query1 = nhemployee1.nhbranchemployee_set.all()
+    query2 = nhemployee2.nhbranchemployee_set.all()
+    if query1.count() == 0 and query2.count() == 0:
+        return 0
+    elif query1.count() == 0:
+        return 1
+    elif query2.count() == 0:
+        return -1
+    return cmp(query1.latest().nhbranch.id, query2.latest().nhbranch.id)
+
 class Tag(models.Model):
     name = models.CharField(unique = True, max_length=20)
     is_deleted = models.BooleanField(editable=False, default=False)
@@ -509,16 +520,6 @@ class NHEmployeeManager(models.Manager):
         nhemployees = list(query)
         nhemployees.sort(nhemployee_sort)
         return nhemployees
-    def nhemployee_sort(nhemployee1, nhemployee2):
-        query1 = nhemployee1.nhbranchemployee_set.all()
-        query2 = nhemployee2.nhbranchemployee_set.all()
-        if query1.count() == 0 and query2.count() == 0:
-            return 0
-        elif query1.count() == 0:
-            return 1
-        elif query2.count() == 0:
-            return -1
-        return cmp(query1.latest().nhbranch.id, query2.latest().nhbranch.id)
 
 class EmployeeBase(Person):
     pid = models.PositiveIntegerField(ugettext('pid'), unique=True)
