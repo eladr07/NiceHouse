9217805e7fc5a39118ad1c47db0bcb99de251e55
Elad Reuveni <eladr07@gmail.com>
2009-12-29 20:23:48 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@2083 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -2875,39 +2875,42 @@ def employeesalary_season_total_expenses(request):
                                            'division_type':division_type_id})
     current = date(from_year, from_month, 1)
     end = date(to_year, to_month, 1)
-    if division_type_id == DivisionType.Marketing:
-        employees = list(Employee.objects.exclude(work_end__isnull = False, work_end__lt = current))
-        model = EmployeeSalary
-    else:
-        if division_type_id == DivisionType.NHShoham:
-            query = NHBranchEmployee.objects.filter(nhbranch__id = NHBranch.Shoham)
-        elif division_type_id == DivisionType.NHModiion:
-            query = NHBranchEmployee.objects.filter(nhbranch__id = NHBranch.Modiin)
-        elif division_type_id == DivisionType.NHNesZiona:
-            query = NHBranchEmployee.objects.filter(nhbranch__id = NHBranch.NesZiona)
-        query = query.exclude(end_date__isnull=False, end_date__lt = current)
-        employees = map(lambda x: x.nhemployee, query)
-        model = NHEmployeeSalary
+    if division_type_id:
+        if division_type_id == DivisionType.Marketing:
+            employees = list(Employee.objects.exclude(work_end__isnull = False, work_end__lt = current))
+            model = EmployeeSalary
+        else:
+            if division_type_id == DivisionType.NHShoham:
+                query = NHBranchEmployee.objects.filter(nhbranch__id = NHBranch.Shoham)
+            elif division_type_id == DivisionType.NHModiin:
+                query = NHBranchEmployee.objects.filter(nhbranch__id = NHBranch.Modiin)
+            elif division_type_id == DivisionType.NHNesZiona:
+                query = NHBranchEmployee.objects.filter(nhbranch__id = NHBranch.NesZiona)
+            query = query.exclude(end_date__isnull=False, end_date__lt = current)
+            employees = map(lambda x: x.nhemployee, query)
+            model = NHEmployeeSalary
 
-    attrs = ['neto', 'loan_pay', 'check_amount', 'income_tax', 'national_insurance', 'health', 'pension_insurance', 
-             'vacation', 'convalescence_pay', 'bruto', 'employer_national_insurance', 'employer_benefit',
-             'compensation_allocation', 'bruto_with_employer']
-    for attr in attrs:
-        for e in employees:
-            setattr(e, 'total_' + attr, 0)
-            
-    while current <= end:
-        salaries = model.objects.filter(year = current.year, month = current.month)
-        for salary in salaries:
-            employee_index = employees.index(salary.get_employee())
-            if not employee_index: continue
-            employee = employees[employee_index]
-            for attr in attrs:
-                add = getattr(salary, attr, 0)
-                old_value = getattr(employee, 'total_' + attr)
-                setattr(employee, 'total_' + attr, old_value + add)
-        current = date(current.month == 12 and current.year + 1 or current.year,
-                       current.month == 12 and 1 or current.month + 1, 1)
+        attrs = ['neto', 'loan_pay', 'check_amount', 'income_tax', 'national_insurance', 'health', 'pension_insurance', 
+                 'vacation', 'convalescence_pay', 'bruto', 'employer_national_insurance', 'employer_benefit',
+                 'compensation_allocation', 'bruto_with_employer']
+        for attr in attrs:
+            for e in employees:
+                setattr(e, 'total_' + attr, 0)
+                
+        while current <= end:
+            salaries = model.objects.filter(year = current.year, month = current.month)
+            for salary in salaries:
+                employee_index = employees.index(salary.get_employee())
+                if not employee_index: continue
+                employee = employees[employee_index]
+                for attr in attrs:
+                    add = getattr(salary, attr, 0)
+                    old_value = getattr(employee, 'total_' + attr)
+                    setattr(employee, 'total_' + attr, old_value + add)
+            current = date(current.month == 12 and current.year + 1 or current.year,
+                           current.month == 12 and 1 or current.month + 1, 1)
+    else:
+        employees = []
             
     return render_to_response('Management/employeesalary_season_total_expenses.html', 
                               { 'employees':employees, 'start':date(from_year, from_month, 1), 'end':date(to_year, to_month, 1),
