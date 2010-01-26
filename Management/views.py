2af6943db6b6a6b29146a75cffb2f80cb3a66c7e
Elad Reuveni <eladr07@gmail.com>
2010-01-26 21:55:10 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@2232 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -753,7 +753,7 @@ def demands_all(request):
     
     total_mispaid, total_unpaid, total_nopayment, total_noinvoice = 0,0,0,0
     amount_mispaid, amount_unpaid, amount_nopayment, amount_noinvoice = 0,0,0,0
-    projects = Project.objects.select_related('demands__diffs','invoices','payments')
+    projects = Project.objects.select_related('demands__diffs','demands__invoices','demands__payments')
     for p in projects:
         for d in p.demands_mispaid():
             amount_mispaid += d.get_total_amount()
