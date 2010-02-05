5be5b6242ccf3449add6b17ec8178118e01e849a
Elad Reuveni <eladr07@gmail.com>
2010-02-05 14:44:09 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@2327 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/decorators.py
+++ Management/decorators.py
@@ -1,9 +1,9 @@
 def cache_method(func):
-    def wrapper(self, *args, **kw):
-        cache_key = (args, kw)
+    def wrapper(self, *args):
+        cache_key = args
         if not hasattr(self, '_method_cache'):
             self._method_cache = {}
         if cache_key in self._method_cache:
             return self._method_cache[cache_key]
-        self._method_cache = func(self, *args, **kw)
+        self._method_cache = func(self, *args)
     return wrapper 
