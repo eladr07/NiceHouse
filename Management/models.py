df0bca9ff507015551f5161f9c51284c5f519e5d
Elad Reuveni <eladr07@gmail.com>
2010-02-27 14:19:05 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@2533 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -1921,8 +1921,8 @@ class Demand(models.Model):
         return query
     @cache_method
     def get_excluded_sales(self):
-        q = models.Q(salecancel__isnull=True) | models.Q(commission_include=False)
-        query = Sale.objects.filter(contractor_pay__year = self.year, contractor_pay__month = self.month,
+        q = models.Q(salecancel__isnull=False) | models.Q(commission_include=True)
+        query = Sale.objects.filter(q, contractor_pay__year = self.year, contractor_pay__month = self.month,
                                     house__building__project = self.project)
         if self.project.commissions.commission_by_signups:
             query = query.order_by('house__signups__date')
