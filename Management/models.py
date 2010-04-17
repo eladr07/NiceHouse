a85cabeebc93c5a43ea8511f8d6654ee6a141db2
Elad Reuveni <eladr07@gmail.com>
2010-04-17 17:26:39 +0000

    organized classes
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@2738 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -8,6 +8,7 @@ from decimal import InvalidOperation
 from django.db.backends.dummy.base import IntegrityError
 from django.db.models import Avg, Max, Min, Count, Sum
 from decorators import cache_method
+from managers import *
 
 Salary_Types = (
                 (None, u'לא ידוע'),
@@ -41,22 +42,6 @@ def clone(from_object, save):
     else:
         new_object = from_object.__class__(**args)
         return new_object
-    
-def nhemployee_sort(nhemployee1, nhemployee2):
-    query1 = nhemployee1.nhbranchemployee_set.all()
-    query2 = nhemployee2.nhbranchemployee_set.all()
-    if query1.count() == 0 and query2.count() == 0:
-        return 0
-    elif query1.count() == 0:
-        return 1
-    elif query2.count() == 0:
-        return -1
-    return cmp(query1.latest().nhbranch.id, query2.latest().nhbranch.id)
-
-class SeasonManager(models.Manager):
-    def range(self, from_year, from_month, to_year, to_month):
-        q = models.Q(year = from_year, month__gte = from_month) | models.Q(year__gt = from_year, year__lt = to_year) | models.Q(year = to_year, month__lte = to_month)
-        return self.filter(q)
     
 class Tag(models.Model):
     name = models.CharField(unique = True, max_length=20)
@@ -173,12 +158,6 @@ class ProjectDetails(models.Model):
     class Meta:
         db_table = 'ProjectDetails'
 
-class ProjectManager(models.Manager):
-    def active(self):
-        return self.filter(end_date=None, is_marketing=True)
-    def archive(self):
-        return self.exclude(end_date=None)
-
 class Project(models.Model):
     details = models.OneToOneField('ProjectDetails', editable=False, null=True)
     initiator = models.CharField(ugettext('initiator'), max_length=30)
@@ -291,29 +270,6 @@ class Storage(models.Model):
         unique_together = ('building', 'num')
         ordering = ['num']
 
-class HouseQuerySet(models.query.QuerySet):
-    def sold(self):
-        q = models.Q(is_sold = True) | models.Q(sales__salecancel__isnull = True)
-        return self.filter(q).annotate(sales_num = Count('sales')).filter(sales_num = 1)
-    def signed(self):
-        return self.filter(signups__cancel__isnull = False).annotate(signups_num = Count('signups')).filter(signups_num = 1)
-    def avalible(self):
-        q = models.Q(is_sold = False) & models.Q(sales__salecancel__isnull = True) & models.Q(signups__cancel__isnull = True)
-        return self.filter(q).annotate(sales_num = Count('sales'), signups_num = Count('signups')).filter(sales_num = 0, signups_num = 0)
-
-
-class HouseManager(models.Manager):
-    use_for_related_fields = True
-    
-    def sold(self):
-        return self.get_query_set().sold()
-    def signed(self):
-        return self.get_query_set().signed()
-    def avalible(self):
-        return self.get_query_set().avalible()
-    def get_query_set(self):
-        return HouseQuerySet(self.model)
-    
 class House(models.Model):
     building = models.ForeignKey('Building', related_name='houses',verbose_name = ugettext('building'), editable=False)
     type = models.ForeignKey('HouseType', verbose_name=ugettext('asset_type'))
@@ -512,25 +468,7 @@ class EmploymentTerms(models.Model):
     tax_deduction_date = models.DateField(ugettext('tax_deduction_date'), null=True, blank=True)
     class Meta:
         db_table='EmploymentTerms'
-
-class EmployeeManager(models.Manager):
-    def active(self):
-        return self.filter(work_end = None)
-    def archive(self):
-        return self.exclude(work_end = None)
     
-class NHEmployeeManager(models.Manager):
-    def active(self):
-        query = self.filter(work_end = None)
-        nhemployees = list(query)
-        nhemployees.sort(nhemployee_sort)
-        return nhemployees
-    def archive(self):
-        query = self.exclude(work_end = None)
-        nhemployees = list(query)
-        nhemployees.sort(nhemployee_sort)
-        return nhemployees
-
 class EmployeeBase(Person):
     pid = models.PositiveIntegerField(ugettext('pid'), unique=True)
     birth_date = models.DateField(ugettext('birth_date'))
@@ -722,13 +660,6 @@ class NHCommission(models.Model):
         return '/nhcbi/%s' % self.id
     class Meta:
         db_table = 'NHCommission'
-
-class NHBranchEmployeeManager(models.Manager):
-    def month(self, year, month):
-        start_date = date(year, month, 1)
-        end_date = date(month == 12 and year + 1 or year, month == 12 and 1 or month + 1, 1)
-        q = models.Q(start_date__lt = start_date) & (models.Q(end_date__isnull = True) | models.Q(end_date__gte = end_date))
-        return self.filter(q)
 
 class NHBranchEmployee(models.Model):
     nhbranch = models.ForeignKey('NHBranch', verbose_name = ugettext('nhbranch'))
@@ -1679,20 +1610,6 @@ class ProjectCommission(models.Model):
     class Meta:
         db_table = 'ProjectCommission'
 
-class InvoiceQuerySet(models.query.QuerySet):
-    def total_amount_offset(self):
-        invoices_amount = self.aggregate(Sum('amount'))['amount__sum'] or 0
-        offsets_amount = self.aggregate(Sum('offset__amount'))['offset__amount__sum'] or 0
-        return invoices_amount + offsets_amount
-    
-class InvoiceManager(models.Manager):
-    use_for_related_fields = True
-    
-    def total_amount_offset(self):
-        return self.get_query_set().total_amount_offset()
-    def get_query_set(self):
-        return InvoiceQuerySet(self.model)
-
 class Invoice(models.Model):
     num = models.IntegerField(ugettext('invoice_num'), unique=True, null=True, blank=True)
     creation_date = models.DateField(auto_now_add = True)
@@ -1728,18 +1645,6 @@ class PaymentType(models.Model):
     class Meta:
         db_table = 'PaymentType'
 
-class PaymentQuerySet(models.query.QuerySet):
-    def total_amount(self):
-        return self.aggregate(Sum('amount'))['amount__sum'] or 0
-    
-class PaymentManager(models.Manager):
-    use_for_related_fields = True
-    
-    def total_amount(self):
-        return self.get_query_set().total_amount()
-    def get_query_set(self):
-        return PaymentQuerySet(self.model)
-    
 class Payment(models.Model):
     num = models.IntegerField(ugettext('check_num'), null=True, blank=True)
     support_num = models.IntegerField(ugettext('support_num'), null=True, blank=True)
@@ -1781,44 +1686,6 @@ class DemandStatus(models.Model):
         db_table = 'DemandStatus'
         get_latest_by = 'date'
 
-class DemandQuerySet(models.query.QuerySet):
-    def total_sales_commission(self):
-        return self.aggregate(Sum('sales_commission'))['sales_commission__sum'] or 0
-    def total_sale_count(self):
-        return self.aggregate(Sum('sale_count'))['sale_count__sum'] or 0
-    def noinvoice(self):
-        query = self.annotate(invoices_num = Count('invoices'), payments_num = Count('payments'))
-        return query.filter(invoices_num = 0, payments_num__gt = 0, force_fully_paid = False)
-    def nopayment(self):
-        query = self.annotate(invoices_num = Count('invoices'), payments_num = Count('payments'))
-        return query.filter(invoices_num__gt = 0, payments_num = 0, force_fully_paid = False)
-    
-class DemandManager(SeasonManager):
-    use_for_related_fields = True
-    
-    def noinvoice(self):
-        return self.get_query_set().noinvoice()
-    def nopayment(self):
-        return self.get_query_set().nopayment()
-    def current(self):
-        now = Demand.current_month()
-        return self.filter(year = now.year, month = now.month)
-    
-    def get_query_set(self):
-        return DemandQuerySet(self.model)
-
-class DemandDiffQuerySet(models.query.QuerySet):
-    def total_amount(self):
-        return self.aggregate(Sum('amount'))['amount__sum'] or 0
-    
-class DemandDiffManager(models.Manager):
-    use_for_related_fields = True
-    
-    def total_amount(self):
-        return self.get_query_set().total_amount()
-    def get_query_set(self):
-        return DemandDiffQuerySet(self.model)
-    
 class DemandDiff(models.Model):
     demand = models.ForeignKey('Demand', editable=False, related_name='diffs')
     type = models.CharField(ugettext('diff_type'), max_length=30, help_text=u'קבועה, משתנה, בונוס, קיזוז או לבחירתך')
@@ -2479,18 +2346,6 @@ class SaleCancel(SaleMod):
     class Meta:
         db_table = 'SaleCancel'
 
-class SaleQuerySet(models.query.QuerySet):
-    def total_price(self):
-        return self.aggregate(Sum('price'))['price__sum'] or 0
-    def total_price_final(self):
-        return self.aggregate(Sum('price_final'))['price_final__sum'] or 0
-    
-class SaleManager(models.Manager):
-    use_for_related_fields = True
-    
-    def get_query_set(self):
-        return SaleQuerySet(self.model)
-
 class Sale(models.Model):
     demand = models.ForeignKey('Demand', related_name='sales', editable=False)
     house = models.ForeignKey('House', related_name = 'sales', verbose_name=ugettext('house'))
@@ -2744,11 +2599,6 @@ class PurposeType(models.Model):
     class Meta:
         db_table = 'PurposeType'
 
-class DivisionTypeManager(models.Manager):
-    def nh_divisions(self):
-        q = models.Q(pk = DivisionType.NHShoham) | models.Q(pk = DivisionType.NHModiin) | models.Q(pk = DivisionType.NHNesZiona)
-        return self.filter(q)
-
 class DivisionType(models.Model):
     Marketing, NHShoham, NHModiin, NHNesZiona = 1,2,3,4
     name = models.CharField(ugettext('name'), max_length=20, unique=True)
@@ -2965,10 +2815,6 @@ class NHActivity(ActivityBase):
     
     class Meta:
         db_table = 'NHActivity'
-
-class ChangeLogManager(models.Manager):
-    def object_changelog(obj):
-        return self.filter(object_type = obj.__class__.name, object_id = obj.id)
 
 class ChangeLog(models.Model):
     date = models.DateTimeField(auto_now_add=True)
---
+++ Management/querysets.py
@@ -0,0 +1,44 @@
+from django.db.models import Avg, Max, Min, Count, Sum
+from django.db import models
+
+class InvoiceQuerySet(models.query.QuerySet):
+    def total_amount_offset(self):
+        invoices_amount = self.aggregate(Sum('amount'))['amount__sum'] or 0
+        offsets_amount = self.aggregate(Sum('offset__amount'))['offset__amount__sum'] or 0
+        return invoices_amount + offsets_amount
+    
+class PaymentQuerySet(models.query.QuerySet):
+    def total_amount(self):
+        return self.aggregate(Sum('amount'))['amount__sum'] or 0
+    
+class DemandDiffQuerySet(models.query.QuerySet):
+    def total_amount(self):
+        return self.aggregate(Sum('amount'))['amount__sum'] or 0
+    
+class DemandQuerySet(models.query.QuerySet):
+    def total_sales_commission(self):
+        return self.aggregate(Sum('sales_commission'))['sales_commission__sum'] or 0
+    def total_sale_count(self):
+        return self.aggregate(Sum('sale_count'))['sale_count__sum'] or 0
+    def noinvoice(self):
+        query = self.annotate(invoices_num = Count('invoices'), payments_num = Count('payments'))
+        return query.filter(invoices_num = 0, payments_num__gt = 0, force_fully_paid = False)
+    def nopayment(self):
+        query = self.annotate(invoices_num = Count('invoices'), payments_num = Count('payments'))
+        return query.filter(invoices_num__gt = 0, payments_num = 0, force_fully_paid = False)
+
+class SaleQuerySet(models.query.QuerySet):
+    def total_price(self):
+        return self.aggregate(Sum('price'))['price__sum'] or 0
+    def total_price_final(self):
+        return self.aggregate(Sum('price_final'))['price_final__sum'] or 0
+
+class HouseQuerySet(models.query.QuerySet):
+    def sold(self):
+        q = models.Q(is_sold = True) | models.Q(sales__salecancel__isnull = True)
+        return self.filter(q).annotate(sales_num = Count('sales')).filter(sales_num = 1)
+    def signed(self):
+        return self.filter(signups__cancel__isnull = False).annotate(signups_num = Count('signups')).filter(signups_num = 1)
+    def avalible(self):
+        q = models.Q(is_sold = False) & models.Q(sales__salecancel__isnull = True) & models.Q(signups__cancel__isnull = True)
+        return self.filter(q).annotate(sales_num = Count('sales'), signups_num = Count('signups')).filter(sales_num = 0, signups_num = 0)
