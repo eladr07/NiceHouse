836f0c0baa215d63fe6bfe12065776e61b18e40e
Elad Reuveni <eladr07@gmail.com>
2010-04-19 16:31:14 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@2777 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- settings.py
+++ settings.py
@@ -90,17 +90,6 @@ INSTALLED_APPS = (
 )
 
 import logging
-import logging.handlers
+import logging.config
 
-# create logger
-logger = logging.getLogger("commission")
-logger.setLevel(logging.DEBUG)
-# create console handler and set level to debug
-ch = logging.handlers.TimedRotatingFileHandler('logs/commission.log', 'midnight')
-ch.setLevel(logging.DEBUG)
-# create formatter
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-# add formatter to ch
-ch.setFormatter(formatter)
-# add ch to logger
-logger.addHandler(ch)
+logging.config.fileConfig('/var/www/NiceHouse/trunk/logging.conf')
