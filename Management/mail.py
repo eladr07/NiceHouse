aeb32db6615480e112acaa138e13966391707f26
Elad Reuveni <eladr07@gmail.com>
2010-06-26 17:22:38 +0000

    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3112 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/mail.py
+++ Management/mail.py
@@ -27,11 +27,13 @@ def mail(to, cc, bcc, subject, contents, attachments = ()):
             attachment_file = attachment
             
         payload = attachment_file.read()
-        filename = unicode(attachment_file.name)
-        raise TypeError
+        filename = attachment_file.name
+
         part.set_payload(payload)
         Encoders.encode_base64(part)
         part.add_header(u'Content-Disposition', 'attachment; filename="%s"' % filename)
+        h=u'Content-Disposition', 'attachment; filename="%s"' % filename
+        raise TypeError
         msg.attach(part)
 
     mailServer = smtplib.SMTP("smtp.gmail.com", 587)
