243a0579b4a6b81fea44b9ee56b3702fc81edc1a
Elad Reuveni <eladr07@gmail.com>
2010-07-05 17:13:29 +0000

    added worker classes
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3136 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/worker.py
+++ Management/worker.py
@@ -1,25 +1,43 @@
-from Queue import Queue, Empty
-from threading import Event
+import threading
+
+# todo : add logging!!
 
 class Worker:
     
     def __init__(self, func):
         super(Worker, self).__init__()
-        self._queue = Queue()
+        # lock used to make the list synchronized
+        self._lock = threading.Lock()
+        # the list of items
+        self._list = []
+        # the function to run for each item
         self._func = func
-        self._event = Event()
+        #event to activate/pause the worker
+        self._event = threading.Event()
         
     def add(self, item):
-        self._queue.put(item)
+        try:
+            self._lock.acquire()
+            self._list.append(item)
+        finally:
+            self._lock.release()
+            
         self._event.set()
         
     def start(self):
         while (True):
             try:
-                item = self._queue.get()
-                self._func(item)
-            except Empty:
-                self._event.clear()
-                continue
+                # wait for incoming items
+                self._event.wait()
+                
+                try:
+                    self._lock.acquire()
+                    item = self._list.pop(0)
+                    self._lock.release()
+                except IndexError:
+                    self._lock.release()
+                    self._event.clear()
+                else:
+                    self._func(item)
             except:
                 continue
