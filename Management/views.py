ce6202f6f28c117c0271b1e4b3582efbfc9a7006
Elad Reuveni <eladr07@gmail.com>
2010-07-06 22:18:07 +0000

    added workers fix
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3154 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -33,16 +33,22 @@ def calc_salary(salary):
     salary.calculate()
     salary.save()
 
-demand_worker = Worker(calc_demand)
-salary_worker = Worker(calc_salary)
+if not hasattr(settings, 'workers_up'):
+    settings.workers_up = False
 
-demand_thread = Thread(target = lambda: demand_worker.start())
-demand_thread.setDaemon(True)
-demand_thread.start()
-
-salary_thread = Thread(target = lambda: salary_worker.start())
-salary_thread.setDaemon(True)
-salary_thread.start()
+if not settings.workers_up:
+    demand_worker = Worker(calc_demand)
+    salary_worker = Worker(calc_salary)
+    
+    demand_thread = Thread(target = lambda: demand_worker.start())
+    demand_thread.setDaemon(True)
+    demand_thread.start()
+    
+    salary_thread = Thread(target = lambda: salary_worker.start())
+    salary_thread.setDaemon(True)
+    salary_thread.start()
+    
+    settings.workers_up = True
 
 #################
 
