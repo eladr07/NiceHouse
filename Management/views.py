75a203ff3e4fc173a20fa6c6916bdf14aab38c51
Elad Reuveni <eladr07@gmail.com>
2010-07-06 16:52:18 +0000

    temp fix
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3143 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -437,7 +437,7 @@ def demand_function(request,id , function):
 @permission_required('Management.list_demand')
 def demand_calc(request, id):
     # end the revision created by the middleware - i want manual control of the revision
-    reversion.revision.end()
+    #reversion.revision.end()
     
     d = Demand.objects.get(pk=id)
     c = d.project.commissions
