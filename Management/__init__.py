905223eb978af8a92d1a0983a6617ce1c6d76973
Elad Reuveni <eladr07@gmail.com>
2010-07-07 15:40:03 +0000

    added thread names
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3157 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/__init__.py
+++ Management/__init__.py
@@ -18,10 +18,10 @@ def initialize():
     demand_worker = Worker(calc_demand)
     salary_worker = Worker(calc_salary)
     
-    demand_thread = Thread(target = lambda: demand_worker.start())
+    demand_thread = Thread(target = lambda: demand_worker.start(), name='demand thread')
     demand_thread.setDaemon(True)
     demand_thread.start()
     
-    salary_thread = Thread(target = lambda: salary_worker.start())
+    salary_thread = Thread(target = lambda: salary_worker.start(), name='salary thread')
     salary_thread.setDaemon(True)
     salary_thread.start()
