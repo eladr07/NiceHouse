09c491ee89ec5c924cbb878831533f3bc242ceb8
Elad Reuveni <eladr07@gmail.com>
2010-07-07 16:07:29 +0000

    bug-fix
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3166 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/__init__.py
+++ Management/__init__.py
@@ -1,25 +1,33 @@
 import reversion
 from worker import Worker
-from threading import Thread
+from threading import Thread, enumerate
 
-def initialize():
+initialize = True
+
+for thread in enumerate():
+    if thread.name in ['demand thread','salary thread']:
+        initialize = False
+        break
+
+if not initialize:
+    return
+
+@reversion.revision.create_on_success
+def calc_demand(demand):
+    demand.calc_sales_commission()
     
-    @reversion.revision.create_on_success
-    def calc_demand(demand):
-        demand.calc_sales_commission()
-        
-    @reversion.revision.create_on_success    
-    def calc_salary(salary):
-        salary.calculate()
-        salary.save()
-        
-    demand_worker = Worker(calc_demand)
-    salary_worker = Worker(calc_salary)
-    
-    demand_thread = Thread(target = lambda: demand_worker.start(), name='demand thread')
-    demand_thread.setDaemon(True)
-    demand_thread.start()
-    
-    salary_thread = Thread(target = lambda: salary_worker.start(), name='salary thread')
-    salary_thread.setDaemon(True)
-    salary_thread.start()
+@reversion.revision.create_on_success    
+def calc_salary(salary):
+    salary.calculate()
+    salary.save()
+
+demand_worker = Worker(calc_demand)
+salary_worker = Worker(calc_salary)
+
+demand_thread = Thread(target = lambda: demand_worker.start(), name='demand thread')
+demand_thread.setDaemon(True)
+demand_thread.start()
+
+salary_thread = Thread(target = lambda: salary_worker.start(), name='salary thread')
+salary_thread.setDaemon(True)
+salary_thread.start()
