1453f67b9e38e33b7a4b23cd5cb7ac35ea9399d9
Elad Reuveni <eladr07@gmail.com>
2010-10-23 19:24:47 +0000

    added NumberedCanvas
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3364 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/pdf.py
+++ Management/pdf.py
@@ -113,6 +113,30 @@ def sigPara():
 def nhAddr():
     return Image(settings.MEDIA_ROOT + 'images/nh_addr.jpg', 300, 50)
 
+class NumberedCanvas(canvas.Canvas):
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._codes = []
+    def showPage(self):
+        self._codes.append({'code': self._code, 'stack': self._codeStack})
+        self._startPage()
+    def save(self):
+        """add page info to each page (page x of y)"""
+        # reset page counter 
+        self._pageNumber = 0
+        for code in self._codes:
+            # recall saved page
+            self._code = code['code']
+            self._codeStack = code['stack']
+            self.setFont("Helvetica", 7)
+            self.drawRightString(200, 20,
+                "page %(this)i of %(total)i" % {
+                   'this': self._pageNumber+1,
+                   'total': len(self._codes),
+                }
+            )
+            canvas.Canvas.showPage(self)
+
 class ProjectListWriter:
     def __init__(self, projects):
         self.projects = projects
@@ -340,7 +364,7 @@ class EmployeeListWriter:
         story.append(titlePara(u'מצבת עובדים'))
         story.append(Spacer(0, 10))
         story.extend(self.employeeFlows())
-        doc.build(story, self.addFirst, self.addLater)
+        doc.build(story, self.addFirst, self.addLater, NumberedCanvas)
         return doc.canv
 
 class MonthDemandWriter:
