2dadb3d02284c0287c83b4683221b1f9ff28c60b
Elad Reuveni <eladr07@gmail.com>
2010-10-23 20:10:00 +0000

    bug-fix
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3378 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/pdf.py
+++ Management/pdf.py
@@ -140,7 +140,7 @@ class NumberedCanvas(canvas.Canvas):
 class DocumentBase:
     def addLater(self, canv, doc):
         frame1 = Frame(0, 680, 150, 50)
-        frame1.addFromList([datePara()()], canv)
+        frame1.addFromList([datePara()], canv)
         frame2 = Frame(0, 680, 500, 150)
         frame2.addFromList([nhLogo()], canv)
         frame4 = Frame(50, 20, 500, 70)
