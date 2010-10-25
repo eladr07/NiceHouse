533a93c5b2c7ea89b099578d0ba3db2529a4f4eb
Elad Reuveni <eladr07@gmail.com>
2010-10-25 21:41:58 +0000

    bug-fix deduct expenses from price_final
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3411 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/models.py
+++ Management/models.py
@@ -2541,7 +2541,7 @@ class Sale(models.Model):
     sale_date = models.DateField(ugettext('sale_date'))
     price = models.IntegerField(ugettext('sale_price'))
     company_price = models.IntegerField(ugettext('company_price'), null=True, blank=True)
-    include_registration = models.NullBooleanField(ugettext('include_registration'), blank=True,
+    include_registration = models.NullBooleanField(ugettext('include_registration'), blank=True, default = None,
                                                    choices = (
                                                               (None,'לא משנה'),
                                                               (False, 'לא'),
