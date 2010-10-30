b13d915752e5a9efdafd17badbcd85e3d822bb13
Elad Reuveni <eladr07@gmail.com>
2010-10-30 14:25:17 +0000

    bug-fix in commission_madad_bi
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3418 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/__init__.py
+++ Management/__init__.py
@@ -15,19 +15,13 @@ def calc_salary(salary):
 demand_worker = Worker(calc_demand)
 salary_worker = Worker(calc_salary)
 
-try:
-    demand_thread = Thread(target = lambda: demand_worker.start(), name='demand thread')
-    demand_thread.setDaemon(True)
-    demand_thread.start()
-except:
-    pass
+demand_thread = Thread(target = lambda: demand_worker.start(), name='demand thread')
+demand_thread.setDaemon(True)
+demand_thread.start()
 
-try:
-    salary_thread = Thread(target = lambda: salary_worker.start(), name='salary thread')
-    salary_thread.setDaemon(True)
-    salary_thread.start()
-except:
-    pass
+salary_thread = Thread(target = lambda: salary_worker.start(), name='salary thread')
+salary_thread.setDaemon(True)
+salary_thread.start()
 
 ####### logging setup ###############
 
