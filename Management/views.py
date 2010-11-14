4fcf2a5ec159f78147bd76cbd69e7057ed171109
Elad Reuveni <eladr07@gmail.com>
2010-11-14 21:33:40 +0000

    bug fix in gc_view
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3456 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/views.py
+++ Management/views.py
@@ -153,7 +153,7 @@ def gc_view(request):
         gc_entry.referers_to_last = gc.get_referrers(gc_entry.seq)
         gc_entry_list.append(gc_entry)
         
-    gc_entry_list.sort(key=lambda gc_entry: len(gc_entry.seq))
+    gc_entry_list.sort(key=lambda gc_entry: len(gc_entry.seq), reverse=True)
         
     response = '<br>'.join([unicode(gc_entry) for gc_entry in gc_entry_list])
     return HttpResponse(response)
