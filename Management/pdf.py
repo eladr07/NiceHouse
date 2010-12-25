83f94a6c0f5ff989be7be368a9fb83585350aba1
Elad Reuveni <eladr07@gmail.com>
2010-12-25 14:44:55 +0000

    bug-fix
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3505 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/pdf.py
+++ Management/pdf.py
@@ -1186,7 +1186,7 @@ class DemandFollowupWriter(DocumentBase):
     def demandFlows(self):
         headers_names = [u"מס'", u"חודש", u"מכירות\nמס'", u"דרישה\nסכום'", u"חשבונית\nמס'", u"סכום", u"תאריך", u"סכום",  u"תאריך", u"לחשבונית\nדרישה", u"לחשבונית\nצ'ק", u"חשבונית\nזיכוי"]
         groups_names = [u"פרטי דרישה", None, None, None, u"פרטי חשבונית", None, None,  u"פרטי צ'קים", None, u"הפרשי דרישה", None, None]
-        groups_names = [u"הפרשי דרישה", None, None, None,None, u"פרטי צ'קים", None, None, u"פרטי חשבונית", None, u"פרטי דרישה"]
+        groups_names = [u"הפרשי דרישה", None, None, u"פרטי צ'קים", None, u"פרטי חשבונית", None, None, u"פרטי דרישה"]
         headers = [log2vis(name) for name in headers_names]
         groups = [name and log2vis(name) for name in groups_names]
         rows = []
