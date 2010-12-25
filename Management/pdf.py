5140ef3402608ade49129f201298d18a1bc96d8d
Elad Reuveni <eladr07@gmail.com>
2010-12-25 15:35:00 +0000

    added sum row for demand followup PDF
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3512 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/pdf.py
+++ Management/pdf.py
@@ -1185,7 +1185,7 @@ class DemandFollowupWriter(DocumentBase):
         groups_names = [u"הפרשי דרישה", None, None, u"פרטי צ'קים", None, u"פרטי חשבונית", None, None, u"פרטי דרישה"]
         headers = [log2vis(name) for name in headers_names]
         groups = [name and log2vis(name) for name in groups_names]
-        colWidths = [None, None, 30, None, 30, None, 40, None, None, None, None, 30]
+        colWidths = [None, None, 30, None, 30, None, 50, None, None, None, None, 30]
         rows = []
         
         total_amount, total_invoices, total_payments, total_diff_invoice, total_diff_invoice_payment = 0,0,0,0,0
