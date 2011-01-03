bcbf50a4706ff963830f97b072a186079ce63d03
Elad Reuveni <eladr07@gmail.com>
2011-01-03 21:37:42 +0000

    added admin
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3534 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/admin.py
+++ Management/admin.py
@@ -4,23 +4,34 @@ from reversion.admin import VersionAdmin
 from inspect import isclass
 
 admin.site.register(Tag)
-admin.site.register(Project)
-admin.site.register(Employee)
 admin.site.register(Contact)
 admin.site.register(House)
 admin.site.register(HouseVersion)
 admin.site.register(Building)
 admin.site.register(Attachment)
 
-
-def get_fields(model):
+def get_fields_names(model):
     return [field.name for field in model._meta.fields]
 
-class CVarPrecentageAdmin(VersionAdmin):
-    list_display = get_fields(CVarPrecentage)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = get_fields_names(Employee)
+    
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = get_fields_names(Project)
 
+class CVarPrecentageAdmin(VersionAdmin):
+    list_display = get_fields_names(CVarPrecentage)
+    
+class CVarPrecentageFixedAdmin(VersionAdmin):
+    list_display = get_fields_names(CVarPrecentageFixed)
+    
+class CVarAdmin(VersionAdmin):
+    list_display = get_fields_names(CVar)
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(CVarPrecentage, CVarPrecentageAdmin)
-admin.site.register(CVarPrecentageFixed, VersionAdmin)
-admin.site.register(CVar, VersionAdmin)
+admin.site.register(CVarPrecentageFixed, CVarPrecentageFixedAdmin)
+admin.site.register(CVar, CVarAdmin)
 admin.site.register(CByPrice, VersionAdmin)
 admin.site.register(CZilber, VersionAdmin)
