6e056c26d437b33a89474c44d603d7391ce0bcfd
Elad Reuveni <eladr07@gmail.com>
2011-03-09 21:25:21 +0000

    changed translation processing
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3802 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/pdf/table_fields.py
+++ Management/pdf/table_fields.py
@@ -15,7 +15,7 @@ class TableField(object):
         self.is_commaised = is_commaised
         self._format_title()
     def _format_title(self):
-        parts = [log2vis(part) for part in self.title.split(' ')]
+        parts = [log2vis(part) for part in self.title.split()]
         parts.reverse()
         return '\n'.join(parts)
     def format(self, item):
