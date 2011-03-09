7bf06cb51eb447dc57d2e9240a49f1ed5ce44c6f
Elad Reuveni <eladr07@gmail.com>
2011-03-09 20:40:38 +0000

    bug-fix
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@3794 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- Management/pdf/table_fields.py
+++ Management/pdf/table_fields.py
@@ -184,7 +184,8 @@ class SaleDateField(TableField):
     def __init__(self):
         return super(SaleDateField, self).__init__(log2vis(ugettext('pdf_sale_date')), 35)
     def format(self, item):
-        return item.sale_date.strftime('%d/%m/%Y')
+        sale_date = item.sale_date
+        return sale_date and sale_date.strftime('%d/%m/%Y') or ''
     
     class Meta:
         models = (models.Sale,)
