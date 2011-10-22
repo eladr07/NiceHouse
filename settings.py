86a5667599866bcf3a93420436691e447467b266
Elad Reuveni <eladr07@gmail.com>
2011-10-22 15:17:18 +0000

    changed logging to comply with django 1.3
    git-svn-id: http://62.90.102.41/svn/NiceHouse/trunk@4007 1431c75d-6ac1-4f6d-bc36-6b0d284ea005

--- settings.py
+++ settings.py
@@ -112,7 +112,7 @@ LOGGING = {
     'handlers': {
         'hand01': {
             'level':'DEBUG',
-            'class':'FileHandler',
+            'class':'logging.FileHandler',
             'args':('/var/www/NiceHouse/trunk/logs/commission.log', 'a'),
             'formatter': 'form01'
         },
