648157487ae820510ba81c8e6663b006dd1c864b
eladr <elad@nummularii.com>
2016-10-30 18:33:52 +0200

    bug-fix: changed SITE_ROOT to STATIC_ROOT

--- Management/pdf/writers.py
+++ Management/pdf/writers.py
@@ -25,8 +25,9 @@ from salary_table_fields import *
 from styles import *
 
 #register Hebrew fonts
-pdfmetrics.registerFont(TTFont('David', Management.settings.SITE_ROOT + 'fonts/DavidCLM-Medium.ttf'))
-pdfmetrics.registerFont(TTFont('David-Bold', Management.settings.SITE_ROOT + 'fonts/DavidCLM-Bold.ttf'))
+import os
+pdfmetrics.registerFont(TTFont('David', os.path.join(Management.settings.STATIC_ROOT, 'fonts/DavidCLM-Medium.ttf')))
+pdfmetrics.registerFont(TTFont('David-Bold', os.path.join(Management.settings.STATIC_ROOT, 'fonts/DavidCLM-Bold.ttf')))
 pdfmetrics.registerFontFamily('David', normal='David', bold='David-Bold')
 
 def break_to_lines(s):
@@ -57,14 +58,14 @@ def tableCaption(caption=log2vis(u'ולהלן פירוט העסקאות')):
                      ParagraphStyle(name='tableCaption', fontName='David-Bold', fontSize=15,
                                     alignment=TA_CENTER))
 def nhLogo():
-    return Image(Management.settings.SITE_ROOT + 'images/nh_logo.jpg', 300, 50)
+    return Image(os.path.join(Management.settings.SITE_ROOT, 'images/nh_logo.jpg'), 300, 50)
 def sigPara():
     s = log2vis('ברגשי כבוד,') + '<br/>'
     s += log2vis('אלי בר-און')
     return Paragraph(s, ParagraphStyle(name='sig', fontName='David-Bold', fontSize=15,
                                        alignment=TA_LEFT))
 def nhAddr():
-    return Image(Management.settings.SITE_ROOT + 'images/nh_addr.jpg', 300, 50)
+    return Image(os.path.join(Management.settings.SITE_ROOT, 'images/nh_addr.jpg'), 300, 50)
 
 class NumberedCanvas(canvas.Canvas):
     def __init__(self, *args, **kwargs):
