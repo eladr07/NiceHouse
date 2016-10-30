ee815b947eaa973a3e4a7e65750ed67a02df1041
eladr <elad@nummularii.com>
2016-10-30 18:50:36 +0200

    resolved conflict

--- settings.py
+++ settings.py
@@ -51,12 +51,8 @@ MEDIA_ROOT = STATIC_ROOT + "/"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-<<<<<<< HEAD:Management/settings.py
 STATIC_URL = '/site_media/'
 MEDIA_URL = STATIC_URL
-=======
-#MEDIA_URL = 'http://62.90.102.41/site_media/'
->>>>>>> parent of c196158... fixed settings:settings.py
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
