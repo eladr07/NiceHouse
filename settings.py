600db306306b61310e51a4daa51c4e0637e2c99e
Elad <eladr07@gmail.com>
2016-10-30 22:58:53 +0200

    commented out MEDIA

--- settings.py
+++ settings.py
@@ -46,13 +46,13 @@ USE_I18N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 STATIC_ROOT = os.path.join(SITE_ROOT, 'media')
-MEDIA_ROOT = STATIC_ROOT + "/"
+#MEDIA_ROOT = STATIC_ROOT + "/"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 STATIC_URL = '/site_media/'
-MEDIA_URL = STATIC_URL
+#MEDIA_URL = STATIC_URL
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
